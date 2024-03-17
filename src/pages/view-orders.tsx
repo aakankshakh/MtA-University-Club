fbd18caf3f31af3e736a2fc3c603576ca34482c8
aakanksha <akhandwaha@mta.ca>
2024-03-16 21:20:53 -0300

    removed double printing in view-orders page

--- src/pages/view-orders.tsx
+++ src/pages/view-orders.tsx
@@ -13,7 +13,6 @@ const orders : OrdersType = {
 export default function ViewOrders() {
     return (
         <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">Online Orders</h1>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         <Orders orders={orders}/>
       </div>
